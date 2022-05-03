87bee0ceb01128dc13edf9aafacc32a51bb28d00
manjawanjal <104351694+manjawanjal@users.noreply.github.com>
2022-05-03 19:45:53 +0530

    Create EmployeeDetails.ts

---
+++ 02.TypeScript/Day 1/EmployeeDetails.ts
@@ -0,0 +1,20 @@
+var EmpId:number=5464654;
+
+var Ename:string  = "Scott";
+
+var Ejob:string  = "Manager";
+
+var IsPermanentEmp:boolean = true;
+
+var Salary:string  = "INR : 45000";
+
+var DepId:number=10;
+
+
+var strOutput = ` Employee Id :${EmpId}, Employee Name : ${Ename},
+
+Employee Job : ${Ejob}, IsPermanetEmployee : ${IsPermanentEmp},
+
+Employee Salary : ${Salary},Employee DeptID : ${DepId} ; 
+
+console.log(strOutput);
