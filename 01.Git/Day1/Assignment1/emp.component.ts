45f3a45dce9f32011ad6eeebfe12d28b38df1722
manjawanjal <104351694+manjawanjal@users.noreply.github.com>
2022-05-05 11:03:43 +0530

    Add files via upload

---
+++ 01.Git/Day1/Assignment1/emp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-emp',
+  templateUrl: './emp.component.html',
+  styleUrls: ['./emp.component.css']
+})
+export class EmpComponent 
+{
+  public emp_id:number = 101;
+  public emp_name:string = "Manjiri";
+  public emp_job:string = "Developer";
+  public emp_salary:number = 4000;
+  public dept_id:number = 122;
+}
