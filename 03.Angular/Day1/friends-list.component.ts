71b2f6b85774da8e63cfda087187eaa5c2c37d05
manjawanjal <104351694+manjawanjal@users.noreply.github.com>
2022-05-06 10:25:09 +0530

    Add files via upload

---
+++ 03.Angular/Day1/friends-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-friends-list',
+  templateUrl: './friends-list.component.html',
+  styleUrls: ['./friends-list.component.css']
+})
+export class FriendsListComponent implements OnInit 
+{
+  friends_list=[
+    {name:"Ms.Namita"},
+    {name:"Ms.Manisha"},
+    {name:"Ms.Ranjana"},
+    {name:"Ms.Anjana"},
+  ];
+
+  
+
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
